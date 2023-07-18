407808a0503949b0dba9503b9725450f9c155e27
Sibesodebra <mwaledebra1@gmail.com>
2023-07-18 06:32:36 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,7 @@
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
+
 int print_sign(int n)
 {
 if (n > 0)
@@ -17,7 +18,9 @@ _putchar(45);
 return(-1);
 }
 else
-}
+{
 _putchar(48);
-return(0)
+return(0);
+}
+
 }
