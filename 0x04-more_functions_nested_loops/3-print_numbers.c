8e793fcc558c3336f115c5d95ba581ee70e8dc26
Sibesodebra <mwaledebra1@gmail.com>
2023-07-20 04:30:47 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "holberton.h"
+#include "stdio.h"
 /**
 * print_numbers - prints the numbers from 0 to 9 followed by a new line
 */
@@ -8,7 +8,7 @@ void print_numbers(void)
 int i;
 for (i = 0; i <= 9; i++)
 {
-_putchar(i + '0');
+putchar(i + '0');
 }
-_putchar('\n');
+putchar('\n');
 }
