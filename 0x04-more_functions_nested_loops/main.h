44c4547642d16d0dab9afc6bc76dbfd9749dc588
Sibesodebra <mwaledebra1@gmail.com>
2023-07-20 03:43:37 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-main.h
 #ifndef MAIN_H
 #define MAIN_H
 int _isupper(int c);
@@ -12,4 +11,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
+#endif
